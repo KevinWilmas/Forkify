70dacbc39ba4b5dc13e5f71e05622c5b77c279c6
kevinwilmas <kevinwilmas@outlook.com>
2022-09-06 10:28:08 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '91139358-dfce-4e37-8f7a-89e44811f3d2';
+export const MODAL_CLOSE_SEC = 2.5;
